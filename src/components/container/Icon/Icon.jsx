f42da8093d437c38a1d08cbc535dd78a9a527e14
saurabh317 <saurabhsp317@gmail.com>
2023-05-03 21:02:13 +0530

    refactored

--- src/components/container/Icon/Icon.jsx
+++ src/components/container/Icon/Icon.jsx
@@ -5,7 +5,7 @@ const Icon = () => {
         <>
             <div className='icons'>
                 <a href="https://github.com/saurabh317" target="_blank" rel="noreferrer"><FaGithub /></a>
-                <a href="https://www.linkedin.com/in/saurabh-sagar-pandey-9b6a5624b/" target="_blank" rel="noreferrer"><FaLinkedin /></a>
+                <a href="https://www.linkedin.com/in/saurabh-sagar-pandey-9b6a56/" target="_blank" rel="noreferrer"><FaLinkedin /></a>
                 <a href="https://www.instagram.com/_saurabhpandey317/" target="_blank" rel="noreferrer"><FaInstagram /></a>
             </div>
 
